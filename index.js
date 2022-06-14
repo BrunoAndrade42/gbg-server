dc04382dd4a83dbcd7994ed35c842a1e6293bc02
Bruno Andrade <brubisandrade@hotmail.com>
2022-06-14 18:46:57 -0300

    cors-again

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const jwt = require('jsonwebtoken')
 
 app.use(cors())
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
+
 app.use(express.json())
 
 
